9cf8c0f6778931590ad93768e8f0bd843643f261
Kevin Lee <sprint_l@aliyun.com>
2018-03-17 17:13:22 +0800

    feat(全局): 添加 react-native-vector-icons 图标库
    添加图标库
    整理文件结构

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import { TabNavigator } from 'react-navigation';
-import styles from './style';
+import { TabNavigator, TabBarBottom } from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 import Hello from '../components/Hello';
 import SelectionScreen from './selection/index';
@@ -10,12 +11,39 @@ import MemberScreen from './member/index';
 import CartScreen from './cart/index';
 import MineScreen from './mine/index';
 
+interface options {
+  navigation: any
+}
+
 export default TabNavigator({
   Selection: { screen: SelectionScreen },
   Brand: { screen: BrandScreen },
   Member: { screen: MemberScreen },
   Cart: { screen: CartScreen },
   Mine: { screen: MineScreen }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, tintColor }) => {
+      const { routeName } = navigation.state
+      let iconName = ''
+
+      if (routeName === 'Selection') {
+        iconName = `ios-information-circle${focused ? '' : '-outline'}`
+      } else {
+        iconName = `ios-options${focused ? '' : '-outline'}`
+      }
+
+      return <Icon name={iconName} size={25} color={tintColor ? tintColor : undefined} />
+    }
+  }),
+  tabBarOptions: {
+    activeTintColor: 'tomato',
+    inactiveTintColor: 'gray',
+  },
+  tabBarComponent: TabBarBottom,
+  tabBarPosition: 'bottom',
+  animationEnabled: true,
+  swipeEnabled: false,
 })
 
 
